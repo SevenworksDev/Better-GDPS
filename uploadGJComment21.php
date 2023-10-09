89845a52da88f1fada746e87c1879f4b81590b48
Sevenworks <91027492+SevenworksDev@users.noreply.github.com>
2023-10-09 06:59:26 -0700

    Create uploadGJComment21.php

---
+++ uploadGJComment21.php
@@ -0,0 +1,12 @@
+<?php
+  function checkCommentBan($userName) {
+      $bannedUsers = file('./config/comment_bans.txt', FILE_IGNORE_NEW_LINES);
+      return in_array($userName, $bannedUsers);
+  }
+
+  if (checkCommentBan($_POST['userName'])) {
+      die('-10');
+  } else {
+      include "incl/comments/uploadGJComment.php";
+  }
+?>
