a60bb7abbb208b21f7304540165069765816caf3
Sevenworks <91027492+SevenworksDev@users.noreply.github.com>
2023-10-09 06:46:55 -0700

    Create likeGJItem211.php

---
+++ likeGJItem211.php
@@ -0,0 +1,19 @@
+<?php
+if (
+    isset($_POST['gameVersion']) &&
+    isset($_POST['binaryVersion']) &&
+    isset($_POST['gdw']) &&
+    isset($_POST['accountID']) &&
+    isset($_POST['gjp']) &&
+    isset($_POST['uuid']) &&
+    isset($_POST['udid']) &&
+    isset($_POST['like']) &&
+    isset($_POST['itemID']) &&
+    isset($_POST['secret']) &&
+    isset($_POST['type']) &&
+) {
+    include "incl/misc/likeGJItem.php";
+} else {
+    echo "-1";
+}
+?>
