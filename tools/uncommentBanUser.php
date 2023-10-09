d664baa565cd9bdc20b9bb2e9598ef8e5c828598
Sevenworks <91027492+SevenworksDev@users.noreply.github.com>
2023-10-09 06:51:28 -0700

    Create uncommentBanUser.php

---
+++ tools/uncommentBanUser.php
@@ -0,0 +1,26 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $filePath = '../config/banned_users.txt';
+    $bannedUsers = file_get_contents($filePath);
+    $bannedUsers = str_replace($username, '', $bannedUsers);
+    $bannedUsers = trim(preg_replace('/\s+/', ' ', $bannedUsers));
+    file_put_contents($filePath, $bannedUsers);
+    echo 'User successfully unbanned.';
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Unban User</title>
+</head>
+<body>
+    <h1>Unban User</h1>
+    <form method="post" action="process_unban.php">
+        <label for="username">Username: </label>
+        <input type="text" id="username" name="username" required><br><br>
+        <input type="submit" value="Unban User">
+    </form>
+</body>
+</html>
