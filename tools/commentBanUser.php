7f77087128c82555358df2414df8d66596547cae
Sevenworks <91027492+SevenworksDev@users.noreply.github.com>
2023-10-10 15:02:29 -0700

    Update commentBanUser.php

--- tools/commentBanUser.php
+++ tools/commentBanUser.php
@@ -1,9 +1,15 @@
 <?php
+include '../config/bettergdps.php';
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $username = $_POST['username'];
-    $filePath = '../config/comment_bans.txt';
-    file_put_contents($filePath, $username . PHP_EOL, FILE_APPEND | LOCK_EX);
-    echo 'User successfully banned.';
+    if ($_POST['password'] === $password) {
+      $username = $_POST['username'];
+      $filePath = '../config/comment_bans.txt';
+      file_put_contents($filePath, $username . PHP_EOL, FILE_APPEND | LOCK_EX);
+      echo 'User successfully banned.';
+    } else {
+        die("unauthorized");
+    }
 }
 ?>
 
@@ -15,8 +21,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <body>
     <h1>Ban User</h1>
     <form method="post" action="commentBanUser.php">
-        <label for="username">Username: </label>
+        <label for="username">Target Username: </label>
         <input type="text" id="username" name="username" required><br><br>
+        <label for="username">GDPS Password: </label>
+        <input type="text" id="password" name="password" required><br><br>
         <input type="submit" value="Ban User">
     </form>
 </body>
